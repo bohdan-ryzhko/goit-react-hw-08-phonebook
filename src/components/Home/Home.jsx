7b5d08b70c59d346c61f9a0709733fb83437c5d7
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-05-23 13:20:57 +0300

    add Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
+import sass from "./Home.module.scss";
 import { Container } from "components/Container/Container";
+import { Title } from "components/Title/Title";
+import { Typography } from '@mui/material';
 
 export const Home = () => {
 	return (
 		<main className="">
 			<Container>
-				<h1>Home</h1>
+				<Typography sx={{ textAlign: "center" }} variant="h3" component="h2">Home</Typography>
+				<div className={sass.homeInner}>
+					<Typography sx={{ textAlign: "center" }} variant="body2" component="p">Design By</Typography>
+					<Title title="Bohdan Ryzhko" />
+				</div>
 			</Container>
 		</main>
 	)
